524ffc4fdc56ed7c5a6736693e1c2c3125d377ff
christian <fabrik42@gmail.com>
2011-02-05 21:09:25 +0100

    introduced remove for api template fields

--- lib/acts_as_api/api_template.rb
+++ lib/acts_as_api/api_template.rb
@@ -1,11 +1,14 @@
 module ActsAsApi
-  # 
+  #
   class ApiTemplate < Hash
-   
-  def add(val, options = {})    
-    self[(options[:as] || val).to_sym] = val
-  end
-    
-    
+
+    def add(val, options = {})
+      self[(options[:as] || val).to_sym] = val
+    end
+
+    def remove(key)
+      self.delete(key)
+    end
+
   end
 end
