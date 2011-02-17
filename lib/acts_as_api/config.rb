da02240a915f306e930785e1f1ca4e8699399adb
christian <fabrik42@gmail.com>
2011-02-17 22:56:39 +0100

    reworked config module

--- lib/acts_as_api/config.rb
+++ lib/acts_as_api/config.rb
@@ -1,40 +1,31 @@
 module ActsAsApi
 
   module Config
-
-    # The accepted response formats
-    # Default is +[:xml, :json]+
-    ACCEPTED_API_FORMATS = [:xml, :json]
-
-    # Holds formats that should be dasherized
-    DASHERIZE_FOR = [:xml]
-
-    # Holds references to formats that need
-    # to get added an additional root node
-    # with the name of the model.
-    ADD_ROOT_NODE_FOR = [:json]
     
-    ALLOW_JSONP_CALLBACK = false
+    class << self
 
-    # The default name of a root node of a response
-    # if no root paramter is passed in render_for_api
-    # and the gem is not able to determine a root name
-    # automatically
-    DEFAULT_ROOT = :record
+      # The accepted response formats
+      # Default is +[:xml, :json]+
+      attr_accessor_with_default :accepted_api_formats, [:xml, :json]
+      
+      # Holds formats that should be dasherized      
+      attr_accessor_with_default :dasherize_for, [:xml]
+      
+      # Holds references to formats that need
+      # to get added an additional root node
+      # with the name of the model.      
+      attr_accessor_with_default :add_root_node_for, [:json]
+      
+      # The default name of a root node of a response
+      # if no root paramter is passed in render_for_api
+      # and the gem is not able to determine a root name
+      # automatically      
+      attr_accessor_with_default :default_root, :record
+      
+      attr_accessor_with_default :allow_jsonp_callback, false
 
-    mattr_accessor :accepted_api_formats
-    mattr_accessor :dasherize_for
-    mattr_accessor :add_root_node_for
-    mattr_accessor :default_root
-    mattr_accessor :allow_jsonp_callback    
-
-    @@accepted_api_formats = ACCEPTED_API_FORMATS
-    @@dasherize_for        = DASHERIZE_FOR
-    @@add_root_node_for    = ADD_ROOT_NODE_FOR
-    @@default_root         = DEFAULT_ROOT
-    @@allow_jsonp_callback = ALLOW_JSONP_CALLBACK    
+    end
     
-
   end
 
 end
