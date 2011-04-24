a7e8f9a4c4164d4f60ed6532d6c85bdf22620ff6
Christian Bäuerlein <fabrik42@gmail.com>
2011-04-24 14:04:40 +0200

    enhanced internal variable naming

--- lib/acts_as_api/api_template.rb
+++ lib/acts_as_api/api_template.rb
@@ -2,15 +2,17 @@ module ActsAsApi
   # Represents an api template for a model.
   # This class should not be initiated by yourself, api templates
   # are created by defining them in the model by calling: +api_accessible+.
-  # 
+  #
   # The api template is configured in the block passed to +api_accessible+.
-  # 
+  #
   # Please note that +ApiTemplate+ inherits from +Hash+ so you can use all
   # kind of +Hash+ and +Enumerable+ methods to manipulate the template.
   class ApiTemplate < Hash
 
     # The name of the api template as a Symbol.
     attr_accessor :api_template
+    
+    attr_accessor :include_root
 
     # Returns a new ApiTemplate with the api template name
     # set to the passed template.
@@ -20,7 +22,11 @@ module ActsAsApi
       return t
     end
 
-    # Adds an item to the api template
+    def initialize
+      @include_root = false
+    end
+
+    # Adds a field to the api template
     #
     # The value passed can be one of the following:
     #  * Symbol - the method with the same name will be called on the model when rendering.
@@ -39,38 +45,40 @@ module ActsAsApi
       @options[item_key] = options
     end
 
-    # Removes an item from the template
-    def remove(item)
-      self.delete(item)
+    # Removes a field from the template
+    def remove(field)
+      self.delete(field)
     end
 
-    # Returns the options of an item in the api template
-    def options_for(item)
-      @options[item]
+    # Returns the options of a field in the api template
+    def options_for(field)
+      @options[field]
     end
 
-    # Returns the passed option of an item in the api template
-    def option_for(item, option)
-      @options[item][option] if @options[item]
+    # Returns the passed option of a field in the api template
+    def option_for(field, option)
+      @options[field][option] if @options[field]
     end
-    
+
     # If a special template name for the passed item is specified
     # it will be returned, if not the original api template.
-    def api_template_for(parent, item)
-      return api_template unless parent.is_a? ActsAsApi::ApiTemplate
-      parent.option_for(item, :template) || api_template
+    def api_template_for(fieldset, field)
+      return api_template unless fieldset.is_a? ActsAsApi::ApiTemplate
+      fieldset.option_for(field, :template) || api_template
     end
-    
+
     # Decides if the passed item should be added to
-    # the response.
-    def allowed_to_render?(parent, item, model)
-      return true unless parent.is_a? ActsAsApi::ApiTemplate
+    # the response based on the conditional options passed.
+    def allowed_to_render?(fieldset, field, model)
+      return true unless fieldset.is_a? ActsAsApi::ApiTemplate
       allowed = true
-      allowed = condition_fulfilled?(model, parent.option_for(item, :if)) if parent.option_for(item, :if)
-      allowed = !(condition_fulfilled?(model, parent.option_for(item, :unless))) if parent.option_for(item, :unless)
+      allowed = condition_fulfilled?(model, fieldset.option_for(field, :if)) if fieldset.option_for(field, :if)
+      allowed = !(condition_fulfilled?(model, fieldset.option_for(field, :unless))) if fieldset.option_for(field, :unless)
       return allowed
     end
-    
+
+    # Checks if a condition is fulfilled
+    # (result is not nil or false)
     def condition_fulfilled?(model, condition)
       case condition
       when Symbol
@@ -80,54 +88,62 @@ module ActsAsApi
       end
       !result.nil? && !result.is_a?(FalseClass)
     end
+    
+    def wrap_in_root_node(fieldset, output)
+      return output unless fieldset.is_a?(ActsAsApi::ApiTemplate) && fieldset.include_root
+      Hash[fieldset.include_root, output]
+    end
 
     # Generates a hash that represents the api response based on this
     # template for the passed model instance.
     def to_response_hash(model)
       queue = []
       api_output = {}
-      queue << { :parent =>  api_output, :item => self }
+      queue << { :output =>  api_output, :item => self }
 
       until queue.empty? do
-          leaf = queue.pop
+        leaf = queue.pop
+        fieldset = leaf[:item]
+        fieldset.each do |field, value|
 
-          leaf[:item].each do |k,v|
+          next unless allowed_to_render?(fieldset, field, model)
 
-            next unless allowed_to_render?(leaf[:item], k, model)
-
-            case v
-            when Symbol
-              if model.respond_to?(v)
-                out = model.send v
-              end
-
-            when Proc
-              out = v.call(model)
-
-            when String
-              # go up the call chain
-              out = model
-              v.split(".").each do |method|
-                out = out.send(method.to_sym)
-              end
-
-            when Hash
-              leaf[:parent][k] ||= {}
-              queue << { :parent =>  leaf[:parent][k], :item => v}
-              next
+          case value
+          when Symbol
+            if model.respond_to?(value)
+              out = model.send value
             end
 
-            if out.respond_to?(:as_api_response)
-              sub_template = api_template_for(leaf[:item], k)
-              out = out.send(:as_api_response, sub_template)
+          when Proc
+            out = value.call(model)
+
+          when String
+            # go up the call chain
+            out = model
+            value.split(".").each do |method|
+              out = out.send(method.to_sym)
             end
 
-            leaf[:parent][k] = out
+          when Hash
+            leaf[:output][field] ||= {}
+            queue << { :output =>  leaf[:output][field], :item => value }
+            next
           end
-        end
 
-        api_output
+          if out.respond_to?(:as_api_response)
+            sub_template = api_template_for(fieldset, field)
+            out = out.send(:as_api_response, sub_template)
+          end
+
+          leaf[:output][field] = out
+        end
+        
+        leaf[:output] = wrap_in_root_node(fieldset, leaf[:output])
+        
       end
 
+      api_output
     end
+    
   end
+end
