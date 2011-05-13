058c73000a3d0ef8ca2aad1a29b25642e448c5ea
Christian Bäuerlein <fabrik42@gmail.com>
2011-05-13 13:18:23 +0200

    fixed spec_helper requirement

--- spec/models/active_record_spec.rb
+++ spec/models/active_record_spec.rb
@@ -1,5 +1,4 @@
-#require File.dirname(__FILE__) + '/../spec_helper.rb'
-#require 'spec_helper'
+require 'spec_helper'
 
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_supports, 'supports:'
