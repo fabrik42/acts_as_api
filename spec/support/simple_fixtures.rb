4c2c9ab2e04513e031759cd6f40d0fa9ac05fb67
Christian Bäuerlein <fabrik42@gmail.com>
2011-09-23 19:29:45 +0200

    cleanup whitespace

--- spec/support/simple_fixtures.rb
+++ spec/support/simple_fixtures.rb
@@ -1,5 +1,5 @@
 module SimpleFixtures
-  
+
   def setup_active_record_models
     @orm_for_testing  = :active_record
     @user_model       = User
@@ -7,7 +7,7 @@ module SimpleFixtures
     @profile_model    = Profile
     @untouched_model  = Untouched
 
-    @luke = @user_model.create({ :first_name => 'Luke',      :last_name => 'Skywalker', :age => 25, :active => true  })  
+    @luke = @user_model.create({ :first_name => 'Luke',      :last_name => 'Skywalker', :age => 25, :active => true  })
     @han  = @user_model.create({ :first_name => 'Han',       :last_name => 'Solo',      :age => 35, :active => true  })
     @leia = @user_model.create({ :first_name => 'Princess',  :last_name => 'Leia',      :age => 25, :active => false })
 
@@ -15,22 +15,22 @@ module SimpleFixtures
 
     @destroy_deathstar = @luke.tasks.create({ :heading => "Destroy Deathstar", :description => "XWing, Shoot, BlowUp",  :time_spent => 30,  :done => true })
     @study_with_yoda   = @luke.tasks.create({ :heading => "Study with Yoda",   :description => "Jedi Stuff, ya know",   :time_spent => 60,  :done => true })
-    @win_rebellion     = @luke.tasks.create({ :heading => "Win Rebellion",     :description => "no idea yet...",        :time_spent => 180, :done => false })    
+    @win_rebellion     = @luke.tasks.create({ :heading => "Win Rebellion",     :description => "no idea yet...",        :time_spent => 180, :done => false })
   end
-  
+
   def clean_up_active_record_models
     @user_model.delete_all
     @task_model.delete_all
   end
-  
+
   def setup_mongoid_models
-    @orm_for_testing  = :mongoid    
+    @orm_for_testing  = :mongoid
     @user_model       = MongoUser
     @task_model       = MongoTask
     @profile_model    = MongoProfile
     @untouched_model  = MongoUntouched
-    
-    @luke = @user_model.new({ :first_name => 'Luke',      :last_name => 'Skywalker', :age => 25, :active => true  })  
+
+    @luke = @user_model.new({ :first_name => 'Luke',      :last_name => 'Skywalker', :age => 25, :active => true  })
     @han  = @user_model.new({ :first_name => 'Han',       :last_name => 'Solo',      :age => 35, :active => true  })
     @leia = @user_model.new({ :first_name => 'Princess',  :last_name => 'Leia',      :age => 25, :active => false })
 
@@ -38,17 +38,17 @@ module SimpleFixtures
 
     @destroy_deathstar = @luke.tasks.new({ :heading => "Destroy Deathstar", :description => "XWing, Shoot, BlowUp",  :time_spent => 30,  :done => true })
     @study_with_yoda   = @luke.tasks.new({ :heading => "Study with Yoda",   :description => "Jedi Stuff, ya know",   :time_spent => 60,  :done => true })
-    @win_rebellion     = @luke.tasks.new({ :heading => "Win Rebellion",     :description => "no idea yet...",        :time_spent => 180, :done => false })    
-    
+    @win_rebellion     = @luke.tasks.new({ :heading => "Win Rebellion",     :description => "no idea yet...",        :time_spent => 180, :done => false })
+
     @luke.save!
     @han.save!
-    @leia.save!    
+    @leia.save!
   end
-  
+
   def clean_up_mongoid_models
     @user_model.delete_all
   end
-  
+
   def setup_roflscale_models
     @orm_for_testing  = :vanilla
     @user_model       = VanillaUser
@@ -56,7 +56,7 @@ module SimpleFixtures
     @profile_model    = VanillaProfile
     @untouched_model  = VanillaUntouched
 
-    @luke = @user_model.new({ :first_name => 'Luke',      :last_name => 'Skywalker', :age => 25, :active => true  })  
+    @luke = @user_model.new({ :first_name => 'Luke',      :last_name => 'Skywalker', :age => 25, :active => true  })
     @han  = @user_model.new({ :first_name => 'Han',       :last_name => 'Solo',      :age => 35, :active => true  })
     @leia = @user_model.new({ :first_name => 'Princess',  :last_name => 'Leia',      :age => 25, :active => false })
 
@@ -65,14 +65,14 @@ module SimpleFixtures
     @destroy_deathstar = @task_model.new({ :user => @luke, :heading => "Destroy Deathstar", :description => "XWing, Shoot, BlowUp",  :time_spent => 30,  :done => true })
     @study_with_yoda   = @task_model.new({ :user => @luke, :heading => "Study with Yoda",   :description => "Jedi Stuff, ya know",   :time_spent => 60,  :done => true })
     @win_rebellion     = @task_model.new({ :user => @luke, :heading => "Win Rebellion",     :description => "no idea yet...",        :time_spent => 180, :done => false })
-    
+
     @luke.tasks << @destroy_deathstar << @study_with_yoda << @win_rebellion
   end
-  
+
   def clean_up_roflscale_models
     # nothing to do ;)
-  end  
-  
+  end
+
 end
 
 
