8d3e8c85bef656b2b959a14474e211c24cfe4057
Christian Bäuerlein <fabrik42@gmail.com>
2011-09-23 18:18:33 +0200

    updated rails app in specs to rails 3.1

--- spec/rails_app/app/controllers/application_controller.rb
+++ spec/rails_app/app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter do
-    if params[:orm] == :active_record
+    if params[:orm] == 'active_record'
       @user_model = User
-    elsif params[:orm] == :mongoid
+    elsif params[:orm] == 'mongoid'
       @user_model = MongoUser
     end
-  end
+  end  
 end
