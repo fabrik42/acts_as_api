4e378bd2d181259fe3b68c4ddef2d024ce6fb9a9
Christian Bäuerlein <fabrik42@gmail.com>
2016-10-30 12:46:33 +0100

    removes deprecation warning for old rspec syntax for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,7 @@ require 'rspec/rails'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|file| require file }
+
+RSpec.configure do |config|
+  config.expect_with(:rspec) { |c| c.syntax = :should }
+end
