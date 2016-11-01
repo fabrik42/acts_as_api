295c6401c099c58ca48fad04908a2c34a7f5782a
Christian Bäuerlein <fabrik42@gmail.com>
2016-11-01 17:10:44 +0100

    removes unneeded comment

--- lib/acts_as_api/collection.rb
+++ lib/acts_as_api/collection.rb
@@ -1,6 +1,4 @@
-# The standard ruby Array class is extended by one instance method.
 module ActsAsApi
-
   module Collection
     # The collection checks all its items if they respond to the +as_api_response+ method.
     # If they do, the result of this method will be collected.
