4caff1c1c4393d016a10642fcbd735fbd3ed31b9
Christian Bäuerlein <fabrik42@gmail.com>
2016-11-08 17:01:10 +0100

    fixes jsonp renderer content type

--- spec/support/controller_examples.rb
+++ spec/support/controller_examples.rb
@@ -279,9 +279,9 @@ shared_examples_for "a controller with ActsAsApi responses" do
 
       end
 
-      describe 'Requesting the JSONP content as JavaScript', pending: true do
+      describe 'Requesting the JSONP content as JavaScript' do
         before(:each) do
-          get :index, format: :js, params: { api_template: :name_only, callback: @callback }
+          get :index, format: :json, params: { api_template: :name_only, callback: @callback }
         end
 
         it "should set the content type to JavaScript" do
